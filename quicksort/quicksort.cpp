21c4805de0a35425f6f4792809205b17048c8a63
sabilafiridas <sabilafirdias@gmail.com>
2024-03-28 10:54:27 +0700

    membuat kondisi if dalam looping do di int main

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -92,6 +92,13 @@ int main()
         input();
         q_short(0, n - 1);
         display();
+        cout << "\n\nDo you want to continue? (y/n): ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+
+        system("pause");
+        system("cls");
     }
 }
 
