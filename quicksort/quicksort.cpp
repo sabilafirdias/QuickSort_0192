ab13c985cebe33be6af4a1f6b439b7b85729ca86
sabilafiridas <sabilafirdias@gmail.com>
2024-03-28 10:23:17 +0700

    membuat prosedur input looping for

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -23,6 +23,11 @@ void input() {
     cout << "\n-------------------" << endl;
     cout << "\nEnter Array Element" << endl;
     cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
 int main()
